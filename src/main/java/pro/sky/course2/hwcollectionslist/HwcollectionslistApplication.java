b216f908347df50446ed94880fd939e5788dfa43
user <kozyan94@gmail.com>
2022-09-28 21:50:28 +0300

    hw collections lists 2.5

---
+++ src/main/java/pro/sky/course2/hwcollectionslist/HwcollectionslistApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.hwcollectionslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwcollectionslistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwcollectionslistApplication.class, args);
+    }
+
+}
